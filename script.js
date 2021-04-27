bce78000cadf664bf178af7eb57aa1e28381ec62
AbidemiT <abidemis01@gmail.com>
2021-04-27 06:13:44 +0100

    Infinite Scroll project, integrating unsplash api

---
+++ script.js
@@ -0,0 +1,4 @@
+// Unsplash API
+const count = 10;
+const apiKey = "a2QnU9qmHDONdoQixp7Q-L5j7GFqdkf91f2076xpRUY"
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${10}`
